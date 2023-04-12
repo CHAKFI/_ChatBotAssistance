85b0ea4ff6c480e9949a6273adfc4f81ac5fb3c2
CHAKFI Ahmed <ahmedchakfi1999@gmail.com>
2023-04-12 12:48:32 +0000

    Backend: Import the dialog file and handle the messages & responses

--- backend/app.py
+++ backend/app.py
@@ -9,48 +9,43 @@ from nltk.chat.util import Chat, reflections
 app = Flask(__name__)
 CORS(app)
 
+file_path = 'C:/Users/ahmed/Documents/Github/Web/_ChatBotAssistance/data/chat.csv'
+def load_data(file_path):
+    data = {}
+    with open(file_path, 'r', encoding='ANSI') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # Skip header row
+        for row in reader:
+            user_messages = row[1].split(',')
+            bot_responses = row[2].split(',')
+            for message in user_messages:
+                if message not in data:
+                    data[message] = bot_responses
+    return data
+
+responses = load_data('C:/Users/ahmed/Documents/Github/Web/_ChatBotAssistance/data/chat.csv')
+
 @app.route('/pros', methods=['GET','POST'])
 def chatbot():
-
     if request.method == 'GET':
         return render_template('run.html')
 
-    # load the data from CSV file
-    pairs = []
-    with open('C:/Users/ahmed/Documents/Github/Web/_ChatBotAssistance/data/chat.csv', 'r', encoding='ANSI') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if len(row) > 0:
-                elements = row[0].split(',')
-                if len(elements) >= 3:
-                    # Extracting nested elements from comma-separated values
-                    id = elements[0]
-                    user_message = elements[1].split(',')
-                    bot_response = elements[2]
-                    
-                    pairs.append([id, user_message, bot_response])
-    
     # handle the message and generate a response
-    print("Received request:", request.data)
+    print("Received request:", request.data) # to check if Flask server is receiving the request
     app.logger.info('Received request: %s', request)
     data = request.get_json()
     app.logger.info('Received message: %s', data)
     message = data.get('message')
 
-    print("Received message:", message)
-    
+    print("Received message:", message) # to check if Flask server is receiving the message
 
-    # Find matching user message
-    matching_pairs = [pair for pair in pairs if user_message == pair[1]]
-
-    if matching_pairs:
-        # Select a random response
-        response =  random.choice(matching_pairs)[2]
-        
+    if not message:
+        response = 'Ce message ne peut pas être traité'
     else:
-        response = 'No response found for this message.'
-
-    print("bot response:", response)
+        if message in responses:
+            response = random.choice(responses[message])
+        else:
+            response = "Désolé, je n'ai pas compris, pouvez-vous essayer quelque chose d'autre ?"
     return jsonify({'message': response})
 
 @app.errorhandler(404)    
