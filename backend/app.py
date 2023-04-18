3bb184bf687b0039fe4aa9a9ded4a4c45ae540cc
CHAKFI Ahmed <ahmedchakfi1999@gmail.com>
2023-04-18 11:22:45 +0000

    backend: Training the data (Maching Learning)

--- backend/app.py
+++ backend/app.py
@@ -2,52 +2,53 @@ import csv
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import random
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 app = Flask(__name__)
 CORS(app)
 
-file_path = 'C:/Users/ahmed/Documents/Github/Web/_ChatBotAssistance/data/chat.csv'
-def load_data(file_path):
-    data = {}
-    with open(file_path, 'r', encoding='ANSI') as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader)  # Skip header row
-        for row in reader:
-            user_messages = row[1].split(',')
-            bot_responses = row[2].split(',')
-            for message in user_messages:
-                if message not in data:
-                    data[message] = bot_responses
-    return data
+# Load the chat data from CSV file
+chat_data = pd.read_csv('C:/Users/ahmed/Documents/Github/Web/_ChatBotAssistance/data/chat.csv', encoding='ANSI')
 
-responses = load_data('C:/Users/ahmed/Documents/Github/Web/_ChatBotAssistance/data/chat.csv')
+# Extract user messages and bot responses from chat data
+user_messages = chat_data['MESSAGE'].tolist()
+bot_responses = chat_data['RESPONSE'].tolist()
 
-@app.route('/pros', methods=['GET','POST'])
+# Train a simple Naive Bayes classifier
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(user_messages)
+clf = MultinomialNB()
+clf.fit(X, bot_responses)
+
+@app.route('/pros', methods=['GET', 'POST'])
 def chatbot():
     if request.method == 'GET':
         return render_template('run.html')
 
     # handle the message and generate a response
-    print("Received request:", request.data) # to check if Flask server is receiving the request
-    app.logger.info('Received request: %s', request)
     data = request.get_json()
-    app.logger.info('Received message: %s', data)
     message = data.get('message')
 
+    app.logger.info('Received request: %s', request)
+    print("Received request:", request.data) # to check if Flask server is receiving the request
+    app.logger.info('Received message: %s', data)
     print("Received message:", message) # to check if Flask server is receiving the message
 
     if not message:
         response = 'Ce message ne peut pas être traité'
     else:
-        message = message.lower()
-        if message in responses:
-            response = random.choice(responses[message])
+        X_message = vectorizer.transform([message])
+        predicted_response = clf.predict(X_message)
+        if predicted_response:
+            response = predicted_response[0]
         else:
             response = "Désolé, je n'ai pas compris, pouvez-vous essayer quelque chose d'autre ?"
     return jsonify({'message': response})
 
-@app.errorhandler(404)    
-def invalid_route(e): 
+@app.errorhandler(404)
+def invalid_route(e):
     return "Invalid route (-_-)."
 
 if __name__ == '__main__':
